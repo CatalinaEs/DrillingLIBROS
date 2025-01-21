d4d5b545288cb7a426cfb040e670e6de74f4fc1c
CatalinaEs <mca.espinoza@gmail.com>
2025-01-21 19:43:30 -0300

    Descripción de los cambios realizados

--- book/admin.py
+++ book/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Libro
+from .models import Libro, Libro1
 # Register your models here.
 class LibroAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'valor', 'valoracion')  # Mostrar título, valor y valoración
+    list_display = ('titulo', 'valoracion', 'valor')  # Mostrar título, valoración y valor
     list_filter = ('valoracion', 'fecha_modificacion')  # Filtros por valoración y fecha de modificación
     readonly_fields = ('fecha_creacion', 'fecha_modificacion')  # Campos de solo lectura
 
 admin.site.register(Libro, LibroAdmin)
+
+
+class LibroAdmin1(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'valoracion', 'obtener_valoracion')  # Mostrar el tipo de valoración
+
+admin.site.register(Libro1, LibroAdmin1)
--- book/models.py
+++ book/models.py
@@ -10,3 +10,21 @@ class Libro(models.Model):
 
     def __str__(self):
         return self.titulo
+    
+    
+class Libro1(models.Model):
+    titulo = models.CharField(max_length=100)
+    autor = models.CharField(max_length=100)
+    valoracion = models.DecimalField(max_digits=5, decimal_places=2)  # Este es el campo de valoración
+
+    def __str__(self):
+        return self.titulo
+
+    # Campo dinámico para mostrar la valoración
+    def obtener_valoracion(self):
+        if self.valoracion < 1000:
+            return 'Baja'
+        elif 1000 <= self.valoracion <= 2500:
+            return 'Media'
+        else:
+            return 'Alta'
